5e8f873aaab5b96aa4ae42022bc11a7bfa3d933f
wogur0824 <wogur0824@naver.com>
2022-05-13 18:53:14 +0900

    의존관계 주입 방법 1. 생성자 주입  ·이름 그대로 생성자를 통해서 의존 관계를 주입 받는 방법이다.  ·지금까지 우리가 진행했던 방법이 바로 생성자 주입이다.  ·특징    -생성자 호출시점에 딱 1번만 호출되는 것이 보장된다. 불변, 필수 의존관계에 사용  ·생성자가 딱 1개만 있을 경우, @Autowired 어노테이션을 생략할 수 있다.  ·단 생성자가 2개 이상부터는 어떤 생성자를 주입시킬지 @Autowired로 지정을 해주어야 한다.
    2. 수정자 주입(setter 주입)
     ·setter라 불리는 필드의 값을 변경하는 수정자 메서드를 통해서 의존관계를 주입하는 방법이다.
     ·특징
        -선택, 변경 가능성이 있는 의존관계에 사용
        -자바빈 프로퍼티 규약의 수정자 메서드 방식을 사용하는 방법이다.
     ·@Autowired 의 기본 동작은 주입할 대상이 없으면 오류가 발생한다. 주입할 대상이 없어도 동작하게 하려면 @Autowired(required = false) 로 지정하면 된다.

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -13,21 +13,45 @@ import org.springframework.stereotype.Component;
 public class OrderServiceImpl implements OrderService{
 
     // memberrepository에서 회원을 찾아야 됨
-    private final MemberRepository memberRepository;
+//    private final MemberRepository memberRepository;
     // 회원 등급이 뭔지를 알아야되서 discountpolicy에서 찾아야 됨
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
 //    private  final DiscountPolicy discountPolicy = new RateDiscountPolicy();
     // 기존에 있던 고정 정액 할인에서 새로운 할인정책인 정률 할인으로 바꾸는 법
     // (추상에만 의존하는게 아니고 구현체에도 의존하므로 DIP, OCP 위반)
 
-    private final DiscountPolicy discountPolicy;
+//    private final DiscountPolicy discountPolicy;
     // 1. orderservice는 interface 즉, 추상에만 의존해야되서 private DiscountPolicy discountPolicy;만 적어준다. (DIP 지킴)
     // 2. final을 붙이지 않은 이유는 final이 붙으면 무조건 값이 할당되어야 하니깐 빼준다.
     // 3. 하지만 이렇게만 설정해놓으면 discountPolicy가 아무 값도 할당이 안되어서 null이여서 오류가 터진다.
 
 
+    // 기존에 있던 private final MemberRepository memberRepository와 private final DiscountPolicy discountPolicy의
+    // final를 삭제해주고 setter라 불리는 수정자 메서드를 통해 의존관계를 주입시켜주는 방법인
+    // 수정자 주입(setter 주입)
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
+
     @Autowired
+    public void setMemberRepository(MemberRepository memberRepository) {
+        System.out.println("2.memberRepository = " + memberRepository);
+        this.memberRepository = memberRepository;
+    }
+
+    @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        System.out.println("2.discountPolicy = " + discountPolicy);
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Autowired
+    // 다양한 의존관계 주입 방법 중에 하나인 생성자 주입
+    // 생성자가 딱 1개만 있을 경우, @Autowired 어노테이션을 생략할 수 있다.
+    // 2개 이상부터는 어떤 생성자를 주입시킬지 @Autowired로 지정을 해주어야 한다.
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        System.out.println("memberRepository = " + memberRepository);
+//        System.out.println("discountPolicy = " + discountPolicy);
+        System.out.println("1.OrderServiceImpl.OrderServiceImpl");
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
