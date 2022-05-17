6a64338932b107c73809f94bdca34938c3ddbcfa
wogur0824 <wogur0824@naver.com>
2022-05-17 18:37:25 +0900

    스프링은 크게 3가지 방법으로 빈 생명주기 콜백을 지원 1. 인터페이스 InitializingBean, DisposableBean   ·InitializingBean 은 afterPropertiesSet() 메서드로 초기화를 지원한다   ·DisposableBean 은 destroy() 메서드로 소멸을 지원한다.   ·인터페이스를 사용하는 초기화, 종료 방법은 스프링 초창기에 나온 방법들이고, 지금은 다음의 더 나은 방법들이 있어서 거의 사용하지 않는다.

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,15 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+// InitializingBean을 implements 하면 초기화 빈이 된다.
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -26,5 +28,19 @@ public class NetworkClient {
     // 서비스를 종료할 때 호출하는 메서드
     public void disconnect() {
         System.out.println("close: " + url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        // 의존관계가 끝나면 호출해주겠다는 메소드
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
